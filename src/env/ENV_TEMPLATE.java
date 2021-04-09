5ad58ffffba70a00f135a49a81a445772ef2a920
Michael Andreas Graversen <michael-graversen@hotmail.com>
2021-04-09 17:24:52 +0200

    Added environment variable

---
+++ src/env/ENV_TEMPLATE.java
@@ -0,0 +1,10 @@
+package env;
+
+public final class ENV_TEMPLATE{
+	public static boolean debug = false;
+
+	public static final String db_host = "";
+	public static final String db_name = "";
+	public static final String db_username = "";
+	public static final String db_password = "!";
+}
