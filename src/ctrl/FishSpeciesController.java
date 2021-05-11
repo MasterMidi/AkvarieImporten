02b6904f755216f43c9367c03276cf7bbc6363ba
magnusBag <42898150+magnusBag@users.noreply.github.com>
2021-05-11 12:41:41 +0200

    FishSpecies

--- src/ctrl/FishSpeciesController.java
+++ src/ctrl/FishSpeciesController.java
@@ -1,7 +1,6 @@
 package ctrl;
 
 import java.util.List;
-
 import db.FishSpeciesDB;
 import db.IFishSpeciesDB;
 import exception.DataAccessException;
@@ -11,14 +10,22 @@ public class FishSpeciesController {
 	private List<FishSpecies> speciesMatches;
 	private IFishSpeciesDB fishSpeciesDb;
 
-public FishSpeciesController() throws DataAccessException {
-	this.fishSpeciesDb = new FishSpeciesDB();
-}
+	public FishSpeciesController() throws DataAccessException {
+		this.fishSpeciesDb = new FishSpeciesDB();
+	}
 
-public List<FishSpecies> searchFishSpecies(String searchInput) {
-	this.speciesMatches = fishSpeciesDb.getFishSpecies(searchInput);
-	return speciesMatches;
-	
-}
-	
+	public List<FishSpecies> searchFishSpecies(String searchInput) {
+		this.speciesMatches = fishSpeciesDb.getFishSpecies(searchInput);
+		return speciesMatches;
+
+	}
+
+	public FishSpecies getFishSpecies(int fishSpeciesId){
+		FishSpecies res = null;
+		res = speciesMatches
+				.parallelStream()
+				.filter((x) -> x.getId()== fishSpeciesId)
+				.findFirst().get();
+		return res;
+	}
 }
