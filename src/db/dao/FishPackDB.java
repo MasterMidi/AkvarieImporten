abbf208368d794c1d49c0bf4e5fba60c7560a801
magnusBag <42898150+magnusBag@users.noreply.github.com>
2021-05-12 11:53:34 +0200

    All kinds og inserts and outskirts

--- src/db/dao/FishPackDB.java
+++ src/db/dao/FishPackDB.java
@@ -3,10 +3,7 @@ package db.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.List;
 
 import db.DBConnection;
 import db.IFishPackDB;
@@ -18,7 +15,7 @@ import model.Period;
 public class FishPackDB implements IFishPackDB {
 	private static final String Q_INSERT_FISH_PACK = "INSERT INTO fish_pack (birthday, fish_specie_id, feeding_plan_id, status) VALUES (?, ?, ?, ?)";
 	private static final String Q_UPDATE_FISH_PACK = "UPDATE fish_pack SET pack_number = ? WHERE id = ?";
-	private static final String Q_INSERT_PERIOD = "INSERT INTO fishpack_period (start_date, aquarium_id, fish_pack_id) VALUES (?,?,?)";
+	private static final String Q_INSERT_PERIOD = "INSERT INTO fish_pack_period (start_date, aquarium_id, fish_pack_id) VALUES (?,?,?)";
 	private PreparedStatement psInsertFishPack;
 	private PreparedStatement psUpdateFishPack;
 	private PreparedStatement psInsertPeriod;
@@ -37,26 +34,28 @@ public class FishPackDB implements IFishPackDB {
 	
 
 	@Override
-	public boolean insertFishPack(FishPack fishPack) throws SQLException, DataAccessException {
+	public void insertFishPack(FishPack fishPack) throws SQLException, DataAccessException {
+		DBConnection.getInstance().startTransaction();
+		
 		prepareStatementFishPack(fishPack);
 		Integer fishPackID = DBConnection.getInstance().executeInsertWithIdentity(psInsertFishPack);
+		
 		String packNumber = fishPack.getSpecies().getName() + fishPackID;
 		psUpdateFishPack.setString(1, packNumber);
-		int periodID = insertPeriod(fishPack.getAquariumPeriods().get(0), fishPackID);
 		psUpdateFishPack.setInt(2, fishPackID);
 		psUpdateFishPack.execute();
 		
+		insertPeriod(fishPack.getAquariumPeriods().get(0), fishPackID);
 		
-		return false;
+		DBConnection.getInstance().commitTransaction();
 	}
 
 
-	private int insertPeriod(Period<Aquarium> period, Integer fishPackID) throws SQLException, DataAccessException {
+	private void insertPeriod(Period<Aquarium> period, Integer fishPackID) throws SQLException, DataAccessException {
 		psInsertPeriod.setDate(1, Date.valueOf(period.getStartDate()));
 		psInsertPeriod.setInt(2, period.getObject().getId());
 		psInsertPeriod.setInt(3, fishPackID);
-		int returnID = DBConnection.getInstance().executeInsertWithIdentity(psInsertPeriod);
-		return returnID;
+DBConnection.getInstance().executeInsertWithIdentity(psInsertPeriod);
 	}
 
 
