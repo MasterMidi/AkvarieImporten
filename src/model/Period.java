5d7853c5927cd67bbf2ce5db5118855ab42cf5a4
magnusBag <42898150+magnusBag@users.noreply.github.com>
2021-05-14 14:33:15 +0200

    period ID and package removal

--- src/model/Period.java
+++ src/model/Period.java
@@ -3,14 +3,26 @@ package model;
 import java.time.LocalDate;
 
 public class Period<T> {
+	private int id;
 	private T object;
 	private LocalDate startDate;
 	private LocalDate endDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public Period(T object, LocalDate startDate) {
 		super();
 		this.object = object;
 		this.startDate = startDate;
+	}
+	public Period(T object, LocalDate startDate, LocalDate endDate) {
+		this(object, startDate);
 	}
 
 	public T getObject() {
