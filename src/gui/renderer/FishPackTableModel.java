7b5e1d038a67b9906b9088ce9c7fd04f8eb9beb9
Michael Andreas Graversen <michael-graversen@hotmail.com>
2021-05-26 22:03:40 +0200

    FOrmatting fix

--- src/gui/renderer/FishPackTableModel.java
+++ src/gui/renderer/FishPackTableModel.java
@@ -9,75 +9,76 @@ import model.FishPack;
 
 public class FishPackTableModel extends AbstractTableModel {
 
-    private static final String[] COL_NAMES = { "Kuld Nr.", "Art", "Akvarie.", "Status", "Lokation", "Foderplan", "Dato" };
-    private List<FishPack> data;
+	private static final String[] COL_NAMES = { "Kuld Nr.", "Art", "Akvarie.", "Status", "Lokation", "Foderplan",
+			"Dato" };
+	private List<FishPack> data;
 
-    public FishPackTableModel() {
-	data = new ArrayList<>();
-    }
-
-    public void setData(List<FishPack> data) {
-	this.data = data;
-	super.fireTableDataChanged();
-    }
-
-    public FishPack getDataAtIndex(int index) {
-	FishPack res = null;
-	if (index >= 0 && index < data.size()) {
-	    res = data.get(index);
+	public FishPackTableModel() {
+		data = new ArrayList<>();
 	}
-	return res;
-    }
 
-    @Override
-    public int getRowCount() {
-	int res = 0;
-	if (data != null) {
-	    res = data.size();
+	public void setData(List<FishPack> data) {
+		this.data = data;
+		super.fireTableDataChanged();
 	}
-	return res;
-    }
 
-    @Override
-    public int getColumnCount() {
-	return COL_NAMES.length;
-    }
-
-    @Override
-    public String getColumnName(int column) {
-	return COL_NAMES[column];
-    }
-
-    @Override
-    public Object getValueAt(int row, int column) {
-	FishPack fishPack = data.get(row);
-	String res = "UNDEFINED";
-	switch (column) {
-	case 0: 
-		res = fishPack.getFishPackNumber();
-		break;  
-	case 1:
-		res = fishPack.getSpecies().getName();
-	    break;
-	case 2:
-		res = fishPack.getCurrentAquarium().getNumber();
-		break;
-	case 3:
-		res = String.valueOf(fishPack.getStatus());
-		break;
-	case 4:
-		res = fishPack.getCurrentAquarium().getLocation();
-		break;
-	case 5:
-		res = fishPack.getFeedingPlan().getName();
-		break;
-	case 6:
-		res = fishPack.getBirthDate().toString();
-		break;
-	default:
-	    res = "UNKNOWN COL - CONTACT ADMIN";
-	    break;
+	public FishPack getDataAtIndex(int index) {
+		FishPack res = null;
+		if (index >= 0 && index < data.size()) {
+			res = data.get(index);
+		}
+		return res;
 	}
-	return res;
-    }
+
+	@Override
+	public int getRowCount() {
+		int res = 0;
+		if (data != null) {
+			res = data.size();
+		}
+		return res;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return COL_NAMES.length;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return COL_NAMES[column];
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		FishPack fishPack = data.get(row);
+		String res = "UNDEFINED";
+		switch (column) {
+		case 0:
+			res = fishPack.getFishPackNumber();
+			break;
+		case 1:
+			res = fishPack.getSpecies().getName();
+			break;
+		case 2:
+			res = fishPack.getCurrentAquarium().getNumber();
+			break;
+		case 3:
+			res = String.valueOf(fishPack.getStatus());
+			break;
+		case 4:
+			res = fishPack.getCurrentAquarium().getLocation();
+			break;
+		case 5:
+			res = fishPack.getFeedingPlan().getName();
+			break;
+		case 6:
+			res = fishPack.getBirthDate().toString();
+			break;
+		default:
+			res = "UNKNOWN COL - CONTACT ADMIN";
+			break;
+		}
+		return res;
+	}
 }
